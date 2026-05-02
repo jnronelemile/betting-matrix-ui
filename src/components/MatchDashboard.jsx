92240d55f0ccf0e6f9b2309f1db4336aa07e40a0
Jean ronel emile <jeanronelstud@gmail.com>
2026-05-02 15:34:11 -0400

    fix: resolve syntax error missing closing brace in MatchDashboard

--- src/components/MatchDashboard.jsx
+++ src/components/MatchDashboard.jsx
@@ -80,6 +80,7 @@ export function MatchDashboard({ match }) {
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m6 9 6 6 6-6"/></svg>
         </div>
       </div>
+      )}
 
       {/* Desktop Tab Navigation */}
       {!isKillSwitch && (
